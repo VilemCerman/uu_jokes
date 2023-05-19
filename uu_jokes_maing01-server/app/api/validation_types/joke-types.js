5700de243b4ea666c948773da6856482dfc4c376
Vilem <vilem.cerman@seznam.cz>
2023-05-19 11:40:44 +0200

    clean the code

--- uu_jokes_maing01-server/app/api/validation_types/joke-types.js
+++ uu_jokes_maing01-server/app/api/validation_types/joke-types.js
@@ -2,7 +2,8 @@
 
 const jokeCreateDtoInType = shape({
   name: string(3, 255).isRequired(),
-  text: string(3, 4000).isRequired()
+  text: string(3, 4000).isRequired(),
+  categoryId: id().isRequired()
 });
 
 const jokeGetDtoInType = shape({
@@ -10,7 +11,7 @@ const jokeGetDtoInType = shape({
 })
 
 const jokeListDtoInType = shape({
-  sortBy: oneOf(["name", "rating"]),
+  sortBy: oneOf(["name", "category"]),
   order: oneOf(["asc", "desc"]),
   pageInfo: shape({
     pageIndex: integer(),
@@ -26,11 +27,5 @@ const jokeUpdateDtoInType = shape({
   id: id().isRequired(),
   name: uu5String(255),
   text: uu5String(4000),
-  categoryIdList: array(id(), 10),
-  image: binary()
+  categoryId: id()
 })
-
-const jokeGetImageDataDtoInType = shape ({
-  image: code().isRequired(),
-  contentDisposition: oneOf(["inline", "attachment"])
-});
