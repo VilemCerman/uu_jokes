8c090d6b411230ee090bd99c6d1c5e8dd42d9868
Vilem <vilem.cerman@seznam.cz>
2023-05-25 10:00:54 +0200

    require category name on joke/create

--- uu_jokes_maing01-server/app/api/validation_types/joke-types.js
+++ uu_jokes_maing01-server/app/api/validation_types/joke-types.js
@@ -3,7 +3,7 @@
 const jokeCreateDtoInType = shape({
   name: string(3, 255).isRequired(),
   text: string(3, 4000).isRequired(),
-  categoryName: string(2, 255)
+  categoryName: string(2, 255).isRequired()
 });
 
 const jokeGetDtoInType = shape({
