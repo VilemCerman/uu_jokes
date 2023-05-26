2a1593f77c58615dace1b25354dcadd7c8c216af
Vilem <vilem.cerman@seznam.cz>
2023-05-26 11:12:02 +0200

    add getByCategory method

--- uu_jokes_maing01-server/app/dao/joke-mongo.js
+++ uu_jokes_maing01-server/app/dao/joke-mongo.js
@@ -1,8 +1,8 @@
-const {UuObjectDao} = require("uu_appg01_server").ObjectStore;
+const { UuObjectDao } = require("uu_appg01_server").ObjectStore;
 
 class JokeMongo extends UuObjectDao {
   async createSchema() {
-    await super.createIndex({awid: 1, _id: 1}, {unique: true});
+    await super.createIndex({ awid: 1, _id: 1 }, { unique: true });
   }
 
   async create(uuObject) {
@@ -15,22 +15,44 @@ class JokeMongo extends UuObjectDao {
 
   async listByVisibility(awid, visibility, dtoIn = {}) {
     let sort = {
-      [dtoIn.sortBy]: dtoIn.order === "asc" ? 1 : -1
+      [dtoIn.sortBy]: dtoIn.order === "asc" ? 1 : -1,
     };
-    return await super.find({awid}, dtoIn.pageInfo, sort);
+    return await super.find({ awid }, dtoIn.pageInfo, sort);
   }
 
   async get(awid, id) {
-    return await super.findOne({awid, id});
+    return await super.findOne({ awid, id });
+  }
+
+  async getByCategory(awid, categoryName) {
+    const all = await this.listByVisibility(awid, true);
+    let jokes = [];
+    all.itemList.forEach((joke) => {
+      if (joke.categoryName === categoryName) {
+        jokes.push(joke);
+      }
+    });
+    const result = {
+      itemList: { jokes },
+      pageInfo: {
+        pageIndex: 0,
+        pageSize: 100,
+        total: jokes.length,
+      },
+      uuErrorMap: all.uuErrorMap,
+    };
+
+    console.log("rResult");
+    console.log(result);
+    return result;
   }
 
   async findOneAndUpdate(uuObject) {
-    return await super.findOneAndUpdate({awid: uuObject.awid, id: uuObject.id},
-      uuObject);
+    return await super.findOneAndUpdate({ awid: uuObject.awid, id: uuObject.id }, uuObject);
   }
 
   async delete(awid, id) {
-    return await super.deleteOne({awid, id});
+    return await super.deleteOne({ awid, id });
   }
 }
 
